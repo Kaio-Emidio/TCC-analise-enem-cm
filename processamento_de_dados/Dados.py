a8c5b03ff282c1a8793649e74ac2d91e195d8ce5
Kaio Emidio <144167357+Kaio-Emidio@users.noreply.github.com>
2026-09-19 22:51:41 -0300

    ajuste no nome da variável do caminho do arquivo .parquet

--- processamento_de_dados/Dados.py
+++ processamento_de_dados/Dados.py
@@ -5,7 +5,7 @@ from config import Config
 
 class Dados:
     def ler_ano(ano):
-        caminho_arquivo = f"{Config.PASTA_SAIDA}/microdados_enem_{ano}.parquet"
+        caminho_arquivo = f"{Config.PASTA_SAIDA}/MICRODADOS_ENEM_{ano}.parquet"
         df = pd.read_parquet(caminho_arquivo)
         return df
 
