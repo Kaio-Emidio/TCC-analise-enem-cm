e2ebb63b9c230832a2d6bb5bf7366e7f6c518d6d
vinicius-gab <144168249+vinicius-gab@users.noreply.github.com>
2026-09-21 21:56:53 -0300

    Adição do botão "Aplicar filtros"

--- app.py
+++ app.py
@@ -1,49 +1,59 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import streamlit as st
+from processamento_de_dados.CriarGrafico import CriarGrafico
 from processamento_de_dados.Dados import Dados
 from processamento_de_dados.Filtrar import Filtrar
-from processamento_de_dados.CriarGrafico import CriarGrafico
 
 Filtrar.filtrar_colunas()
 
-st.set_page_config(
-    page_title='Análise Temporal Notas do ENEM',
-    layout='wide')
+st.set_page_config(page_title='Análise Temporal Notas do ENEM', layout='wide')
 
 st.title('Análise Temporal Notas do ENEM')
 
 progress_bar = st.empty()
 status_text = st.empty()
 
-anos = []
-for ano in range(2009, 2025):
-    anos.append(ano)
+anos = list(range(2009, 2025))
 
-with st.sidebar:
+with st.sidebar.form(key='filtros_enem'):
     ano_de_foco = st.selectbox(
         label='Selecione o ano de foco',
         options=anos,
-        index=anos.index(2022)
+        index=anos.index(2022) if 2022 in anos else 0,
     )
 
     ano_inicio, ano_fim = st.slider(
         label='Selecione o intervalo de anos desejados',
         min_value=2009,
         max_value=2024,
-        value=(2009, 2024)
+        value=(2009, 2024),
     )
 
     cidades_selecionadas = st.multiselect(
         label='Selecione os municípios que deseja visualizar',
-        options=['Ceará-Mirim', 'Natal', 'Parnamirim', 'Extremoz', 'São Gonçalo do Amarante', 'Macaíba'],
-        default=[]
+        options=[
+            'Ceará-Mirim',
+            'Natal',
+            'Parnamirim',
+            'Extremoz',
+            'São Gonçalo do Amarante',
+            'Macaíba',
+        ],
+        default=[],
     )
 
     disciplina_selecionada = st.selectbox(
         label='Selecione a disciplina que deseja visualizar',
-        options=['Ciências da Natureza', 'Ciências Humanas', 'Linguagens e Códigos', 'Matemática', 'Redação']
+        options=[
+            'Ciências da Natureza',
+            'Ciências Humanas',
+            'Linguagens e Códigos',
+            'Matemática',
+            'Redação',
+        ],
     )
+    st.form_submit_button(label='Aplicar Filtros', type='primary')
 
 st.subheader(f'Média das notas em cada disciplina em {ano_de_foco}')
 dados_ano_selec = Dados.ler_ano(ano_de_foco)
@@ -52,44 +62,51 @@ estatistica_ano_selec = Dados.estatistica(dados_ano_selec)
 colunas = st.columns(5)
 
 with colunas[0]:
-    st.metric(label='Média Linguagens e Códigos', value=estatistica_ano_selec['Linguagens e Códigos']['media'])
+    st.metric(
+        label='Média Linguagens e Códigos',
+        value=estatistica_ano_selec['Linguagens e Códigos']['media'],
+    )
 with colunas[1]:
-    st.metric(label='Média Ciências Humanas', value=estatistica_ano_selec['Ciências Humanas']['media'])
+    st.metric(
+        label='Média Ciências Humanas',
+        value=estatistica_ano_selec['Ciências Humanas']['media'],
+    )
 with colunas[2]:
-    st.metric(label='Média Matemática', value=estatistica_ano_selec['Matemática']['media'])
+    st.metric(
+        label='Média Matemática',
+        value=estatistica_ano_selec['Matemática']['media'],
+    )
 with colunas[3]:
-    st.metric(label='Média Ciências da Natureza', value=estatistica_ano_selec['Ciências da Natureza']['media'])
+    st.metric(
+        label='Média Ciências da Natureza',
+        value=estatistica_ano_selec['Ciências da Natureza']['media'],
+    )
 with colunas[4]:
-    st.metric(label='Média Redação', value=estatistica_ano_selec['Redação']['media'])
-
-# Conforme o ano selecionado, os valores das notas mudam conforme o ano
+    st.metric(
+        label='Média Redação',
+        value=estatistica_ano_selec['Redação']['media'],
+    )
 
 st.header('Gráficos')
 
 st.subheader('Exibição temporal')
-
 anos_selecionados = list(range(ano_inicio, ano_fim + 1))
 
-
 grafico_linha = CriarGrafico.linha(
-    cidades=cidades_selecionadas,
-    anos=anos_selecionados
+    cidades=cidades_selecionadas, anos=anos_selecionados
 )
-st.plotly_chart(grafico_linha, width='stretch')
+st.plotly_chart(grafico_linha, use_container_width=True)
 
 st.subheader('Exibição Focalizada')
-
 grafico_radar = CriarGrafico.radar(
-    cidades=cidades_selecionadas,
-    ano=ano_de_foco
+    cidades=cidades_selecionadas, ano=ano_de_foco
 )
-st.plotly_chart(grafico_radar, width='stretch')
+st.plotly_chart(grafico_radar, use_container_width=True)
 
 st.subheader('Distribuição das Notas')
-
 grafico_violino = CriarGrafico.violino(
     cidades=cidades_selecionadas,
     ano=ano_de_foco,
-    disciplina=disciplina_selecionada
+    disciplina=disciplina_selecionada,
 )
-st.plotly_chart(grafico_violino, width='stretch')
+st.plotly_chart(grafico_violino, use_container_width=True)
